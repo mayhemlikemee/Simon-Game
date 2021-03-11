4277223e5e1a95736a4f8365f57023fa007a8634
mayhemlikemee <61032130+mayhemlikemee@users.noreply.github.com>
2021-03-10 23:48:27 -0600

    dot

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ function nextSequence() {
     // Animate a flash to the button selected
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   
-    // Sound for the button colour selected
+    // Sound for the button colour selected 
     var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     audio.play();
 };
