ca3a56966f9d960deec1e3fecebfd6ad85c4eb0d
mayhemlikemee <61032130+mayhemlikemee@users.noreply.github.com>
2021-03-10 23:46:20 -0600

    initial commit

---
+++ game.js
@@ -0,0 +1,17 @@
+gamePattern = [];
+
+buttonColours = ["red", "blue", "green", "yellow"];
+
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+  
+    // Animate a flash to the button selected
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  
+    // Sound for the button colour selected
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+};
