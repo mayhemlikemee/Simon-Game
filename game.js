a1f4d694cfa07427e10cb74abc4144a80fd9d7f1
mayhemlikemee <61032130+mayhemlikemee@users.noreply.github.com>
2021-03-13 06:00:46 -0600

    Added event listener  and flash effect

--- game.js
+++ game.js
@@ -1,17 +1,58 @@
 gamePattern = [];
 
+userClickedPattern = [];
+
 buttonColours = ["red", "blue", "green", "yellow"];
 
 
 function nextSequence() {
+
+    // Random number between 0-3
     var randomNumber = Math.floor(Math.random() * 4);
+
+    // Matches number to a color based on indexing
     var randomChosenColour = buttonColours[randomNumber];
+
+    // Adds the random color selected to the empty game pattern
     gamePattern.push(randomChosenColour);
   
-    // Animate a flash to the button selected
+    // Animates a flash to the button selected
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   
-    // Sound for the button colour selected 
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+
+};
+
+// Event listener on the buttons to detect a click
+$(".btn").click(function() {
+    
+    // Selects the chosen color's ID attribute
+    userChosenColour = $(this).attr("id");
+
+    // Pushes it onto user clicked array
+    userClickedPattern.push(userChosenColour);
+
+
+    // Plays sound corresponding to button color
+    playSound(userChosenColour);
+
+    animatePress(userChosenColour);
+});
+
+
+function playSound(name) {
+        // Sound for the button colour selected 
+        var audio = new Audio("sounds/" + name + ".mp3");
+        audio.play();
+};
+
+// Flashes the button that was clicked
+function animatePress(currentColour) {
+    
+    // Adds class pressed to selected button
+    $("#" + currentColour).addClass("pressed");
+
+    // Removes the class after 100ms to create the "flash" effect
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
 };
